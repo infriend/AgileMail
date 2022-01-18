19962580f2684b579947ff2a3393fbf99d3e00eb
liuyuan <liuyuan@magus.ink>
2022-01-18 16:37:30 +0800

    first commit

---
+++ src/main/java/edu/agiledev/agilemail/AgilemailApplication.java
@@ -0,0 +1,13 @@
+package edu.agiledev.agilemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilemailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgilemailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/agiledev/agilemail/AgilemailApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agiledev.agilemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilemailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
