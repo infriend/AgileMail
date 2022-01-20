bdd731530305db57cc8f2990dba5304c7e1c5637
Nagisa-Kaworu <740283386@qq.com>
2022-01-20 16:15:17 +0800

    frontend

---
+++ src/main/resources/agile-frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Route, Switch, Redirect} from 'react-router-dom'
+import './App.css';
+import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage'
+import React from 'react';
+
+const App = () =>{
+  return(
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default App;
