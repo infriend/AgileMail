daf8593adf4ee1ea4b5d94efaeb204ac1505b2f9
infriend <stive0118@sina.com>
2022-04-17 18:36:33 +0800

    feat: database add new table for address book

---
+++ doc/amail0417.sql
@@ -0,0 +1,84 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50736
+ Source Host           : localhost:3306
+ Source Schema         : amail
+
+ Target Server Type    : MySQL
+ Target Server Version : 50736
+ File Encoding         : 65001
+
+ Date: 17/04/2022 18:31:27
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_addressbook
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_addressbook`;
+CREATE TABLE `sys_addressbook`  (
+  `id` varchar(36) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `contact_email` varchar(36) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`contact_email`, `id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_addressbook
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_email_account
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_email_account`;
+CREATE TABLE `sys_email_account`  (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账户id',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱用户名',
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱密码',
+  `domain` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱域名',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '删除标识',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '邮箱账户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_email_account
+-- ----------------------------
+INSERT INTO `sys_email_account` VALUES ('1529078053739827201', 'sqltest1@test.com', 'sqltest', 'test.com', '0');
+INSERT INTO `sys_email_account` VALUES ('1529078053739827202', 'sqltest2@test.com', 'sqltest', 'test.com', '0');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '删除标识',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1529235917448024071', '0');
+INSERT INTO `sys_user` VALUES ('1529235917448024072', '0');
+
+-- ----------------------------
+-- Table structure for sys_user_email
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_email`;
+CREATE TABLE `sys_user_email`  (
+  `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+  `email_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '邮箱账户id',
+  PRIMARY KEY (`user_id`, `email_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户和邮箱关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_email
+-- ----------------------------
+INSERT INTO `sys_user_email` VALUES ('1529235917448024071', '1529078053739827201');
+
+SET FOREIGN_KEY_CHECKS = 1;
